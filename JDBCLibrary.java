d61be57f4c07b58bc4fef2d2f1000d2c8c2267e0
liv13004 <liv13004@byui.edu>
2016-06-29 12:20:29 -0600

    Add files via upload
    Manually uploaded file. Not sure why it wasn't pushed by netbeans.

--- JDBCLibrary.java
+++ JDBCLibrary.java
@@ -12,9 +12,15 @@
 ||  Program purpose:  Designed as a user defined Exception.
 */
 
+
+
 // Class imports.
 import java.awt.*;            // Required for AWT widgets.
 import java.awt.event.*;      // Required for AWT events.
+import com.mysql.jdbc.Driver;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // ------------------------------ Begin Class --------------------------------/
 
@@ -22,7 +28,12 @@ import java.awt.event.*;      // Required for AWT events.
 public class JDBCLibrary
 {
   // -------------------------- Class Variables ------------------------------/
-
+// JDBC driver name and database URL
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://byuirocketdynamics.org:3306/byuirock_jmessenger";
+   static final String USER = "byuirock_DBAcces";
+   static final String PASS = "{-~gi0fgcI^c";
+   
   // ------------------------- Begin Constructor -----------------------------/
 
   /*
@@ -36,7 +47,9 @@ public class JDBCLibrary
   // -------------------------------------------------------------------------/
 
   // Default constructor extends RuntimeException.
-  public JDBCLibrary() {}
+  public JDBCLibrary(){
+  
+  }
 
   // --------------------------- Begin Methods -------------------------------/
 
@@ -54,9 +67,81 @@ public class JDBCLibrary
   // -------------------------- End Inner Class ------------------------------/
 
   // ------------------------- Begin Static Main -----------------------------/
+public static void main(String[] args) {
+   Connection conn = null;
+   Statement stmt = null;
+  try {
+           //STEP 2: Register JDBC driver
+           Class.forName("com.mysql.jdbc.Driver");
+       } catch (ClassNotFoundException ex) {
+           Logger.getLogger(JDBCLibrary.class.getName()).log(Level.SEVERE, null, ex);
+       }
 
-  // -------------------------- End Static Main ------------------------------/
+      //STEP 3: Open a connection
+      System.out.println("Connecting to database...");
+       try {
+           conn = DriverManager.getConnection(DB_URL,USER,PASS);
+       } catch (SQLException ex) {
+           Logger.getLogger(JDBCLibrary.class.getName()).log(Level.SEVERE, null, ex);
+       }
+        System.out.println("Creating statement...");
+       try {
+           stmt = conn.createStatement();
+       } catch (SQLException ex) {
+           Logger.getLogger(JDBCLibrary.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       
+       String sql;
+      sql = "select * from Users;";
+      ResultSet rs = null;
+       try {
+           rs = stmt.executeQuery(sql);
+           //stmt.executeQuery(sql);
+       } catch (SQLException ex) {
+           Logger.getLogger(JDBCLibrary.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       
+       System.out.println(rs.toString());
+/*
+      //STEP 4: Execute a query
+      System.out.println("Creating statement...");
+       try {
+           stmt = conn.createStatement();
+       } catch (SQLException ex) {
+           Logger.getLogger(JDBCLibrary.class.getName()).log(Level.SEVERE, null, ex);
+       }
+      String sql;
+      sql = "SELECT id, first, last, age FROM Employees";
+      ResultSet rs = null;
+       try {
+           rs = stmt.executeQuery(sql);
+       } catch (SQLException ex) {
+           Logger.getLogger(JDBCLibrary.class.getName()).log(Level.SEVERE, null, ex);
+       }
 
-} // End of JDBCLibrary class.
+       try {
+           //STEP 5: Extract data from result set
+           while(rs.next()){
+               //Retrieve by column name
+               int id  = rs.getInt("id");
+               int age = rs.getInt("age");
+               String first = rs.getString("first");
+               String last = rs.getString("last");
+               
+               //Display values
+               System.out.print("ID: " + id);
+               System.out.print(", Age: " + age);
+               System.out.print(", First: " + first);
+               System.out.println(", Last: " + last);
+           }
+           // -------------------------- End Static Main ------------------------------/
+       } catch (SQLException ex) {
+           Logger.getLogger(JDBCLibrary.class.getName()).log(Level.SEVERE, null, ex);
+       }
+*/
+   } 
+}
+
+// End of JDBCLibrary class.
 
 // ------------------------------- End Class ---------------------------------/
